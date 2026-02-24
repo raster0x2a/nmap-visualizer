9b4056cfc155f41463433e7206a939b88501e63d
raster0x2a <raster0x2a@gmail.com>
2026-02-24 22:51:35 +0900

    fix: set base path to relative for github pages compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 const isElectron = process.env.ELECTRON === 'true'
 
 export default defineConfig({
+    base: './',
     plugins: [
         react(),
         isElectron && electron([
